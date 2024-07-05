bc01e75f685c24da70f999129cff0a6e1b4e6560
Pradipta Shrestha <pradiptashrestha@gmail.com>
2024-07-04 23:17:20 -0500

    spring boot init

---
+++ src/test/java/com/springboot/demo/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
